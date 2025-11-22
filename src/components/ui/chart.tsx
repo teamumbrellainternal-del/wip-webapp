844d19edeb44c35dad264f31c6cad942386c4e2f
thomas-temp <teamumbrellainternal@gmail.com>
2025-11-22 15:23:43 -0800

    fix(format): format chart.tsx component

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -173,7 +173,6 @@ const ChartTooltipContent = React.forwardRef<
         {!nestLabel ? tooltipLabel : null}
         <div className="grid gap-1.5">
           {payload.map((item: any, index: number) => {
-             
             const key = `${nameKey || item.name || item.dataKey || 'value'}`
             const itemConfig = getPayloadConfigFromPayload(config, item, key)
             const indicatorColor = color || item.payload.fill || item.color
@@ -276,7 +275,6 @@ const ChartLegendContent = React.forwardRef<
         )}
       >
         {payload.map((item: any) => {
-           
           const key = `${nameKey || item.dataKey || 'value'}`
           const itemConfig = getPayloadConfigFromPayload(config, item, key)
 
